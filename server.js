2e490f4524bb28024294b48b1529f0b3830d8e50
ImRohan01 <rhshetty01@gmail.com>
2020-08-09 13:21:09 +0530

    Hosting on heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+const port = process.env.port || 8080
+
+app.use(express.static(__dirname))
+
+app.get("/", function(req,res){
+    res.render(index)
+})
+
+app.listen(port, function(){
+    console.log("app running")
+})
